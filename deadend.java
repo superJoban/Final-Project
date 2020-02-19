bc4b0f3ea99dac8ab50104be9a3b90ae75d5aaaa
luna-mcdonald <60760096+luna-mcdonald@users.noreply.github.com>
2020-02-19 10:29:28 -0700

    Add files via upload

---
+++ deadend.java
@@ -0,0 +1,13 @@
+
+public class deadend (playerX, playerY) {
+	
+	//add all barrier coordinates somehow
+	
+	int playerX = get.playerCoorX;
+	int playerY = get.playerCoorY;
+	
+	if (playerX==barrierX and playerY==barrierY) {
+		return false;
+	}
+	
+}
