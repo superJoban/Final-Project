c85cf0d790214338775fbd3ed0d10eef270a9738
tanvi <tanvi@10.0.0.169>
2020-02-24 15:09:01 -0700

    Game screen's obstacles.

---
+++ JavaProject/src/mouse1.java
@@ -0,0 +1,32 @@
+/*
+public class mouse1 {
+
+	public static void main(String[] args) {
+		gameWindow();
+	}
+	public static void gameWindow() {
+		System.out.print(  "__________________________________________________________________________               _________");
+		System.out.print("\n|                                   ______                                |             |        |");
+		System.out.print("\n|                                  |      |                               |             |        |");
+		System.out.print("\n|     __                           |      |_________                      |             |        |");
+		System.out.print("\n|    |  |                          |                |                     |             |        |");
+		System.out.print("\n|    |  |                          |________________|                     |_____________|        |");
+		System.out.print("\n|    |__|              ____________________                                                      |");
+		System.out.print("\n|                      |                  |                                                      |");
+		System.out.print("\n|                      |                  |                                                      |");
+		System.out.print("\n|                      |__________________|                                                      |");
+		System.out.print("\n|                                                  _________________________                     |");
+		System.out.print("\n|      ____                                        |                       |                     |");
+		System.out.print("\n|     |____|                     _______           |                       |                     |");
+		System.out.print("\n|                                |     |           |                       |                     |");
+		System.out.print("\n|                                |_____|           |                       |                     |");
+		System.out.print("\n|                  __________                      |_______________________|                     |");
+		System.out.print("\n|                  |        |                                                                    |");
+		System.out.print("\n|                  |        |                                                                    |");
+		System.out.print("\n|__________________|        |____________________________________________________________________|");
+		
+		
+	}
+
+}
+*/
