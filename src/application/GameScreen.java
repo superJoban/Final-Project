6dc0f2bfccc562891a82050d9ad657d1e5fdb69c
superJoban <60759797+superJoban@users.noreply.github.com>
2020-03-11 22:39:23 -0600

    Comments added to code for Start Screen

--- src/application/GameScreen.java
+++ src/application/GameScreen.java
@@ -4,26 +4,26 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class GameScreen {
+public class GameScreen{
 	private AnchorPane gamePane;
 	private Scene gameScene;
 	private Stage gameStage;
 	private Stage menuStage;
 	
+	//Method for the actual gameplay
 	public void startGame() {
 		gamePane = new AnchorPane();
 		gameScene = new Scene(gamePane, 600, 500);
 		gameStage = new Stage();
 		gameStage.setScene(gameScene);
 		
-		if (StartScreen.pressStart = true) {
-			character.addCharacter(100,100);
-		}
 	}
 
 	public void newGame(Stage menuStage) {
+		//start the game. Empty screen
 		startGame();
 		this.menuStage = menuStage;
+		//Hide menu screen, and show the game play screen
 		this.menuStage.hide();
 		this.gameStage.show();
 		
