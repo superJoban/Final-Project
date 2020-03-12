854eb93a5df0d298bc05f381dccb90ce0eda4978
Chae <chae.matthewc00@gmail.com>
2020-03-11 22:12:47 -0600

    :)

---
+++ FinalProj/src/application/GameScreen.java
@@ -0,0 +1,31 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GameScreen {
+	private AnchorPane gamePane;
+	private Scene gameScene;
+	private Stage gameStage;
+	private Stage menuStage;
+	
+	public void startGame() {
+		gamePane = new AnchorPane();
+		gameScene = new Scene(gamePane, 600, 500);
+		gameStage = new Stage();
+		gameStage.setScene(gameScene);
+		
+		if (StartScreen.pressStart = true) {
+			character.addCharacter(100,100);
+		}
+	}
+
+	public void newGame(Stage menuStage) {
+		startGame();
+		this.menuStage = menuStage;
+		this.menuStage.hide();
+		this.gameStage.show();
+		
+	}
+}
